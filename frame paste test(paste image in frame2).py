eb58bfd742bea6b80cc31fbb7f614a1465579ef0
NBrittingham <spartan2468931@gmail.com>
2015-01-14 08:36:17 -0600

    newest version of code

---
+++ frame paste test(paste image in frame2).py
@@ -0,0 +1,40 @@
+import PIL
+import matplotlib.pyplot as plt # single use of plt is commented out
+import os.path  
+import PIL.ImageDraw  
+
+def get_images(directory=None):
+    """ Returns PIL.Image objects for all the images in directory.
+    
+    If directory is not specified, uses current directory.
+    Returns a 2-tuple containing 
+    a list with a  PIL.Image object for each image file in root_directory, and
+    a list with a string filename for each image file in root_directory
+    """
+    
+    if directory == None:
+        directory = os.getcwd() # Use working directory if unspecified
+        
+    image_list = [] # Initialize aggregaotrs
+    file_list = []
+    
+    directory_list = os.listdir(directory) # Get list of files
+    for entry in directory_list:
+        absolute_filename = os.path.join(directory, entry)
+        try:
+            image = PIL.Image.open(absolute_filename)
+            file_list += [entry]
+            image_list += [image]
+        except IOError:
+            pass # do nothing with errors tying to open non-images
+    return image_list, file_list
+    
+def put_image_in_frame(directory=None):
+    directory = os.path.dirname(os.path.abspath(__file__))
+    frame1 = os.path.join(directory, 'triangleframe.jpg')
+    given_pic_file= os.path.join(directory, 'earth.png')
+    frame1_img = PIL.Image.open(frame1)
+    fig, axes = plt.subplots(1, 2)
+    axes[0].imshow(frame1_img, interpolation='none')
+    
+    frame1_img.paste(given_pic_file, (42, 25), mask=given_pic_file)
